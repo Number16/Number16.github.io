edd28686ccad5c92f419c7c6eadb741c99b413a7
Igor <write.igor@gmail.com>
2023-05-31 17:15:03 +0200

    bingo updated

--- wwdc-bingo/script.js
+++ wwdc-bingo/script.js
@@ -1,21 +1,26 @@
 var imgs_available = [];
 var imgs_shuffled = [];
-var img;
 var imgs = [];
-var generate_btn_tapped = false;
+var centerImg;
 
 var w = 10;
-  var h = 10;
+var h = 10;
 
+function preload() {
+  for (var i = 1; i <= 24; i++) {
+    imgs_available.push(String(i) + '.png');
+  }
+  
+  imgs_shuffled = imgs_available.sort(() => 0.5 - Math.random());
 
+  for (var i = 0; i < 24; i++) {
+    imgs.push(loadImage(imgs_shuffled[i]));
+  }
 
-
-
+  centerImg = loadImage('central.png');
+}
 
 function setup() {
-
-
-
   if (windowWidth * 1.2 > windowHeight) {
     h = min(windowHeight, 600);
     w = h / 1.2;
@@ -25,67 +30,31 @@ function setup() {
   }
 
   createCanvas(w, h);
-
-
-
-  for (var i = 1; i<26; i++) {
-    imgs_available.push(String(i));
-  }
-
-  imgs_shuffled = imgs_available.sort(() => 0.5 - Math.random());
-
-  for (var i = 0; i<25; i++) {
-    imgs.push(loadImage(imgs_shuffled[i] + '.png'));
-  }
-
-
-
-}
-
-function draw() {
-
-  background(255);
-        textSize(15);
-
+  background(0);
+  
+  fill(255); // Set the text color to white
+  textSize(30);
   textAlign(CENTER);
-
+  text('WWDC BINGO', w/2, h/10 + 10);
 
   let a = w / 125.0;
-
   let b = w / 5.0 - w/500;
-
   let c = w / 5.0;
-
   let d = w / 5.0 - a;
 
-
-  if (generate_btn_tapped == true) {
-
-  text('Create your personal WWDC bingo\nat lekskeks.com/wwdc-bingo', c*2.5, c/2);
-       for (var i = 0; i<5; i++) {
+  for (var i = 0; i<5; i++) {
     for (var j = 0; j<5; j++) {
-      image(imgs[5*j + i], a + i*b, c + a + j*b, d, d);
-
+      if(i == 2 && j == 2) {
+        image(centerImg, a + i*b, c + a + j*b, d, d);
+      } else {
+        var img = imgs.shift();
+        imgs.push(img);
+        image(img, a + i*b, c + a + j*b, d, d);
+      }
     }
   }
-  } else {
-      text('Tap to generate your personal WWDC bingo', c*2.5, c);
-
-
-  }
-
-
-
-
-
 }
 
 function touchStarted() {
-  if (generate_btn_tapped == true) {
-      save("bingo.png");
-  } else {
-    generate_btn_tapped = true;
-  }
-
-
+  save("bingo.png");
 }
